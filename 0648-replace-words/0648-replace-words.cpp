0ea47dd8c17040f37711bfa0e1c4c52067a120e1
Junnan Shimizu <67613527+JunnanShimizu@users.noreply.github.com>
2024-05-01 14:45:53 +0200

    Time: 946 ms (6.81%), Space: 627.1 MB (5.10%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        std::vector<std::string> words;
+        std::stringstream ss(sentence);
+        std::string currentWord;
+        
+        while (ss >> currentWord) {
+            words.push_back(currentWord);
+        }
+        
+        for(std::string& word : words){
+            for(std::string dictWord : dictionary){
+                if(word.substr(0, dictWord.length()) == dictWord){
+                    if(dictWord.length() < word.length()){
+                        word = dictWord;
+                    }
+                }
+            }
+        }
+        
+        std::string newSentence = "";
+        for(std::string word : words){
+            newSentence += word + " ";
+        }
+        
+        return newSentence.substr(0, newSentence.length() - 1);
+    }
+};
