0b83a6bda77ab8c27fe33d907cbcbab4b42a22c5
Junnan Shimizu <67613527+JunnanShimizu@users.noreply.github.com>
2024-05-01 12:02:04 +0200

    Time: 0 ms (100.00%), Space: 7.8 MB (8.46%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int index = word.find(ch);
+        
+        if(index == std::string::npos){
+            return word;
+        }
+        
+        string newString = "";
+        for(int i = index; i >= 0; i--){
+            newString = newString + word[i];
+        }
+        
+        return newString + word.substr(index + 1);
+    }
+};
