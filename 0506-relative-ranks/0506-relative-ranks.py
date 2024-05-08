8b75d4e82505a91e773202cb06a765c0a7125736
Junnan Shimizu <67613527+JunnanShimizu@users.noreply.github.com>
2024-05-08 10:09:11 +0200

    Time: 1599 ms (5.05%), Space: 17.6 MB (89.48%) - LeetHub

---
+++ 0506-relative-ranks/0506-relative-ranks.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findRelativeRanks(self, score: List[int]) -> List[str]:
+        answer = [0] * len(score)
+        
+        for i in range(len(score)):
+            max_index = score.index(max(score))
+            if i == 0:
+                answer[max_index] = "Gold Medal"
+            elif i == 1:
+                answer[max_index] = "Silver Medal"
+            elif i == 2:
+                answer[max_index] = "Bronze Medal"
+            else:
+                 answer[max_index] = str(i + 1)
+            score[max_index] = min(score) - 1
+            
+        return answer
